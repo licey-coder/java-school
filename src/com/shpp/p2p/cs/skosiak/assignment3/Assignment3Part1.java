e99565b977bd3ed83a9e567198773104ab9a7510
serhii kosyachenko <kosyachenko@gmail.com>
2022-03-03 09:24:21 +0200

    first D commit

---
+++ src/com/shpp/p2p/cs/skosiak/assignment3/Assignment3Part1.java
@@ -0,0 +1,12 @@
+package com.shpp.p2p.cs.skosiak.assignment3;
+
+import com.shpp.cs.a.console.TextProgram;
+
+public class Assignment3Part1 extends TextProgram {
+    public void run(){
+        print("Hello World!");
+
+    }
+
+
+}
